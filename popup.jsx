614ca5b4f1681f8f753113f53f198f927408f39d
kuko0411 <kuko0411@gmail.com>
2016-01-29 21:47:14 +0100

    Added store functionality

--- popup.jsx
+++ popup.jsx
@@ -1,9 +1,33 @@
+//chrome.storage.sync.set({tabs: []})
+
+function saveTab(tab) {
+    chrome.storage.sync.get({"tabs": []}, function(data){
+        data.tabs.push(tab)
+        chrome.storage.sync.set(data)
+    })
+}
+
+function deleteTab(tab) {
+    chrome.storage.sync.get({"tabs": []}, function(data){
+        var index = data.tabs.lenght + 20
+
+        for (var i=0; i<data.tabs.length; i++){
+            if ((data.tabs[i].id === tab.id) && (data.tabs[i].url === tab.url))
+                index = i
+
+        }
+
+        data.tabs.splice(index, 1)
+        chrome.storage.sync.set(data)
+    })
+}
+
 var TabButton = React.createClass({
     render: function(){
         return (
             <a className="item" id={this.props.tab.id}> 
                 <button className="ui small button" onClick={this.props.onClick}> 
-                    <img src={this.props.tab.favIconUrl} height={15} width={15}></img>  
+                    <img src={this.props.tab.favIconUrl} height={15} width={15}/>  
                       
                     {this.props.tab.title}
                 </button>
@@ -12,7 +36,7 @@ var TabButton = React.createClass({
     },
 })
 
-var TabList = React.createClass({
+var ActiveTabList = React.createClass({
     getInitialState: function(){
         return {tabs:[]}
     },
@@ -22,7 +46,7 @@ var TabList = React.createClass({
             function(tabs) {
                 this.setState({tabs: tabs})
             }.bind(this)
-        )    
+        )  
     },
 
     componentDidMount: function() {
@@ -31,6 +55,46 @@ var TabList = React.createClass({
 
     handleClick: function(tab){
         chrome.tabs.remove(tab.id)
+        this.reload()
+        saveTab(tab)
+    },
+
+    render: function() {
+        var buttons = this.state.tabs.map(function(tab) {
+                return (
+                    <TabButton tab={tab} onClick={this.handleClick.bind(this, tab)}></TabButton>
+                )
+            }.bind(this)
+        )
+
+        return (
+            <div className="ui small vertical list">
+                {buttons}
+            </div>
+        );
+    }
+});
+
+var StoredTabList = React.createClass({
+    getInitialState: function(){
+        return {tabs:[]}
+    },
+
+    reload: function(){
+        chrome.storage.sync.get({tabs: []}, 
+            function(data) {
+                this.setState({tabs: data.tabs})
+            }.bind(this)
+        )  
+    },
+
+    componentDidMount: function() {
+        this.reload()      
+    },
+
+    handleClick: function(tab){
+        deleteTab(tab)
+        chrome.tabs.create({url: tab.url})
         this.reload()
     },
 
@@ -50,8 +114,24 @@ var TabList = React.createClass({
     }
 });
 
+
+var Content = React.createClass({
+    render: function() {
+        return (
+            <div className="ui container">
+                <h1>Open tabs</h1>
+                <ActiveTabList/>
+                <br/>
+                <h1>Stored tabs</h1>
+                <StoredTabList/>
+            </div>
+        )   
+    }
+})
+
+
 ReactDOM.render(
-    <TabList/>,
+    <Content/>,
     document.getElementById('content')
 );
 
