5d28188ff0866a99db157e8e451f4479cd9d31a3
kuko0411 <kuko0411@gmail.com>
2016-06-19 20:01:15 +0200

    Compiled documentation

---
+++ api.js
@@ -0,0 +1,14 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ActiveTabList",
+        "Content",
+        "Standalone",
+        "StoredTabList",
+        "TabButton"
+    ],
+    "modules": [],
+    "allModules": [],
+    "elements": []
+} };
+});
